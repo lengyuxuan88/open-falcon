573cf1183b4b000c05425e30994c6334cb202d01
yang <1@2.com>
2018-01-17 16:58:40 +0800

    modify

--- 60_fans_status.py
+++ 60_fans_status.py
@@ -7,7 +7,9 @@ import time
 hostname = socket.gethostname()
 ipmitool_cmd="ipmitool -I open sdr | grep Fans"
 cmd_result = commands.getstatusoutput(ipmitool_cmd)
-fans_status = cmd_result[1].split("|")[2].strip()
+fans_result = cmd_result[1].split("|")[2].strip()
+if fans_result == "ok":
+    fans_status = 0
 if __name__ == '__main__':
     s = [{"endpoint": hostname, "tags": "", "timestamp": int(time.time()), "metric": "fans.status", "value": fans_status, "counterType": "GAUGE", "step": 60}]
     print json.dumps(s)
