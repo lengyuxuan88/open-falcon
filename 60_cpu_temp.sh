ee7fa90daa141e8ce9a6e708be7edfcdf2de5aa5
yang <1@2.com>
2018-01-19 14:02:32 +0800

    add *

--- 60_cpu_temp.sh
+++ 60_cpu_temp.sh
@@ -2,7 +2,7 @@
 host_name=$(hostname)
 ts=$(date +%s)
 cpu_temp=$(ipmitool -I open sdr | grep CPU | awk '{print $1="cpu_temp""_"$2,$4}')
-echo -e "$cpu_temp" | while read n1 n2
+echo -e "$cpu_temp" | while read name number
 do
-    curl -X POST -d "[{\"metric\": \"$n1\", \"endpoint\": \"${host_name}\", \"timestamp\": $ts,\"step\": 60,\"value\": $n2,\"counterType\": \"GAUGE\"}]" http://127.0.0.1:1988/v1/push
+    curl -X POST -d "[{\"metric\": \"$name\", \"endpoint\": \"${host_name}\", \"timestamp\": $ts,\"step\": 60,\"value\": $number,\"counterType\": \"GAUGE\"}]" http://127.0.0.1:1988/v1/push
 done
--- 60_power.sh
+++ 60_power.sh
@@ -3,16 +3,16 @@ host_name=`hostname`
 name=$(ipmitool -I open sdr list | grep 'Power Supply'| awk '{print $1"_"$2"_"$3,$8}')
 #echo $name
 ts=`date +%s`
-echo -e "$name" | while read c1 c2 
+echo -e "$name" | while read power status 
 do
-    for i in "$c2"
+    for i in "$status"
     do
         if [ "$i" == "ok" ];then 
-            c2=0
+            status=0
 	else
-	    c2=1
+	    status=1
         fi
-        curl -X POST -d "[{\"metric\": \"$c1\", \"endpoint\": \"${host_name}\", \"timestamp\": $ts,\"step\": 60,\"value\": $c2,\"counterType\": \"GAUGE\"}]" http://127.0.0.1:1988/v1/push
+        curl -X POST -d "[{\"metric\": \"$power\", \"endpoint\": \"${host_name}\", \"timestamp\": $ts,\"step\": 60,\"value\": $status,\"counterType\": \"GAUGE\"}]" http://127.0.0.1:1988/v1/push
     done
 done
 
