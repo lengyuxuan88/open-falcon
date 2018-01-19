3a02f8314a6cfdcde2311eb6155283a14f0441c7
yang <1@2.com>
2018-01-19 14:04:57 +0800

    modify cpu_status

--- 60_cpu_status.sh
+++ 60_cpu_status.sh
@@ -2,12 +2,12 @@
 host_name=$(hostname)
 ts=$(date +%s)
 cpu_status=$(ipmitool -I open sdr | grep CPU | awk '{print $1="cpu_status""_"$2,$8}')
-echo -e "$cpu_status" | while read n1 n2
+echo -e "$cpu_status" | while read cpu status
 do
-    if [ "$n2" == "ok" ];then
-        n2=0
+    if [ "$status" == "ok" ];then
+        status=0
     else
-        n2=1
+        status=1
     fi
-    curl -X POST -d "[{\"metric\": \"$n1\", \"endpoint\": \"${host_name}\", \"timestamp\": $ts,\"step\": 60,\"value\": $n2,\"counterType\": \"GAUGE\"}]" http://127.0.0.1:1988/v1/push
+    curl -X POST -d "[{\"metric\": \"$cpu\", \"endpoint\": \"${host_name}\", \"timestamp\": $ts,\"step\": 60,\"value\": $status,\"counterType\": \"GAUGE\"}]" http://127.0.0.1:1988/v1/push
 done
