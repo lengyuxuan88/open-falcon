731a7958033eb089b0634873c9aab017a0c4947b
yang <1@2.com>
2018-01-22 10:33:06 +0800

    modify cpu_temp

--- 60_cpu_temp.sh
+++ 60_cpu_temp.sh
@@ -2,7 +2,7 @@
 host_name=$(hostname)
 ts=$(date +%s)
 cpu_temp=$(ipmitool -I open sdr | grep CPU | awk '{print $1="cpu_temp""_"$2,$4}')
-echo -e "$cpu_temp" | while read name number
+echo -e "$cpu_temp" | while read cpu temp
 do
-    curl -X POST -d "[{\"metric\": \"$name\", \"endpoint\": \"${host_name}\", \"timestamp\": $ts,\"step\": 60,\"value\": $number,\"counterType\": \"GAUGE\"}]" http://127.0.0.1:1988/v1/push
+    curl -X POST -d "[{\"metric\": \"$cpu\", \"endpoint\": \"${host_name}\", \"timestamp\": $ts,\"step\": 60,\"value\": $temp,\"counterType\": \"GAUGE\"}]" http://127.0.0.1:1988/v1/push
 done
