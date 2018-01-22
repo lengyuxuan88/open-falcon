9504f1834987b91f40c4af90348b6596d208b67c
yang <1@2.com>
2018-01-22 10:16:02 +0800

    del files

--- 60_url_status.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-http_status=$(curl -I -m 10 -o /dev/null -s -w %{http_code}"\r\n" 127.0.0.1/index.html)
-host_name=$(hostname)
-ts=$(date +%s)
-curl -X POST -d "[{\"metric\": \"url_status\", \"endpoint\": \"${host_name}\", \"timestamp\": $ts,\"step\": 60,\"value\": ${http_status},\"counterType\": \"GAUGE\"}]" http://127.0.0.1:1988/v1/push
--- 60_user_number.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-login_number=$(uptime | awk -F ',' '{print $3}'| awk '{print $1}')
-host_name=$(hostname)
-ts=$(date +%s)
-curl -X POST -d "[{\"metric\": \"login_user\", \"endpoint\": \"${host_name}\", \"timestamp\": $ts,\"step\": 60,\"value\": ${login_number},\"counterType\": \"GAUGE\"}]" http://127.0.0.1:1988/v1/push
